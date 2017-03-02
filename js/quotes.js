c876b714394ea02fd104bdc5bb97f47760aee776
Ethan <elowenthal16@student.hausner.com>
2017-03-01 17:55:43 -0800

    first commit

---
+++ js/quotes.js
@@ -0,0 +1,7 @@
+var quotes = [
+"I felt like making a website.",
+"I'm in high school. And I like to program.",
+"Welcome to my site",
+"If you don't have a website, you're not cool.",
+"If you have an idea of what to put here, send me an email on my contact page"
+];
