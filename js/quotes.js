239809a47ad454b1933c465c9f3659d0a907eb25
Ethan <elowenthal16@student.hausner.com>
2019-06-06 19:58:04 -0700

    new-site!

--- js/quotes.js
+++
@@ -1,8 +0,0 @@
-var quotes = [
-"I felt like making a website.",
-"I'm in high school, and I like to program.",
-"Welcome to my site",
-"If you don't have a website, you're not cool.",
-"If you have an idea of what to put here, send me an email on my contact page",
-"Over 30,500 lines of code!"
-];
