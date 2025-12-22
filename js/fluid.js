f06287dabf00d94d6adcf1950bb86fcb1d08457a
Ethan Lowenthal <elowenthal@psiquantum.com>
2025-12-22 11:18:21 -0800

    try this

--- js/fluid.js
+++ js/fluid.js
@@ -1208,6 +1208,8 @@ window.addEventListener("load", () => {
     // const c = document.getElementById('fluid-canvas');
 
     // document.getElementById('debug').innerHTML = `Canvas height: ${c.height}, window height: ${c.height}, window.innerHeight: ${window.innerHeight}, window.outerHeight: ${window.outerHeight}, htmlCanvas height: ${htmlCanv.height / window.devicePixelRatio}`;
+    
+    document.getElementById('debug').innerHTML = 'fix';
 
     const scaleFactor = (window.outerHeight + window.innerHeight) / window.outerHeight;
 
@@ -1216,7 +1218,7 @@ window.addEventListener("load", () => {
       0, window.scrollY * window.devicePixelRatio,
       htmlCanv.width,htmlCanv.height,
       0, 0, 
-      htmlCanv.width,htmlCanv.height * scaleFactor 
+      htmlCanv.width,htmlCanv.height / window.devicePixelRatio 
     );
     fluidConfig.PAUSED = true;
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, tempCanvas);
